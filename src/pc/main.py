c855842c775f63f35fcf93b5ce9120b5ef42aaee
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-06 17:34:02 -0500

    fix constants

--- src/pc/main.py
+++ src/pc/main.py
@@ -1,8 +1,8 @@
 from sensor_bt import SensorBT
 
-_ANALOG_UUID = const("000025a8-0000-1000-8000-00805f9b34fb")
-_DEVICE_NAME = const("temp-sense")
-_SAMPLING_DURATION = const(1200)
+_ANALOG_UUID = "000025a8-0000-1000-8000-00805f9b34fb"
+_DEVICE_NAME = "temp-sense"
+_SAMPLING_DURATION = 1200
 
 if __name__ == "__main__":
     bt = SensorBT(device_name=_DEVICE_NAME, analog_uuid=_ANALOG_UUID)
