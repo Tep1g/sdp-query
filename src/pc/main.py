c0db8f0dc260062f5933668a2d9660d16d42e848
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-06 17:48:36 -0500

    fix bug by awaiting bt method call

--- src/pc/main.py
+++ src/pc/main.py
@@ -1,9 +1,13 @@
+import asyncio
 from sensor_bt import SensorBT
 
 _ANALOG_UUID = "000025a8-0000-1000-8000-00805f9b34fb"
 _DEVICE_NAME = "temp-sense"
-_SAMPLING_DURATION = 1200
+_SAMPLING_DURATION = 5
+
+async def main():
+    bt = SensorBT(device_name=_DEVICE_NAME, analog_uuid=_ANALOG_UUID)
+    await bt.collect_measurements(duration=_SAMPLING_DURATION)
 
 if __name__ == "__main__":
-    bt = SensorBT(device_name=_DEVICE_NAME, analog_uuid=_ANALOG_UUID)
-    bt.collect_measurements(duration=_SAMPLING_DURATION)
+    asyncio.run(main())
