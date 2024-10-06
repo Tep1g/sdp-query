8cd62f7a54005d57afdfd6d37b4c5e5f971faa31
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-06 17:27:21 -0500

    instantiate SensorBT object and make method call

--- src/pc/main.py
+++ src/pc/main.py
@@ -1,2 +1,9 @@
-_ANALOG_UUID = "000025a8-0000-1000-8000-00805f9b34fb"
-_SENSOR_NAME = "temp-sense"
+from sensor_bt import SensorBT
+
+_ANALOG_UUID = const("000025a8-0000-1000-8000-00805f9b34fb")
+_DEVICE_NAME = const("temp-sense")
+_SAMPLING_DURATION = const(1200)
+
+if __name__ == "__main__":
+    bt = SensorBT(device_name=_DEVICE_NAME, analog_uuid=_ANALOG_UUID)
+    bt.collect_measurements(duration=_SAMPLING_DURATION)
