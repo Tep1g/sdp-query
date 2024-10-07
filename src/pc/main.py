5b6059e6eafa6ce6c921ea362e8a0086f120afb6
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-07 00:10:28 -0500

    get sampling duration from argparse

--- src/pc/main.py
+++ src/pc/main.py
@@ -1,13 +1,17 @@
+import argparse
 import asyncio
 from client_bt import ClientBT
 
 _ANALOG_UUID = "000025a8-0000-1000-8000-00805f9b34fb"
 _DEVICE_NAME = "temp-sense"
-_SAMPLING_DURATION = 5
 
 async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--sampling_duration', type=int, required=True)
+    sampling_duration = parser.parse_args().sampling_duration
+
     bt = ClientBT(device_name=_DEVICE_NAME, analog_uuid=_ANALOG_UUID)
-    await bt.receive_measurements(duration=_SAMPLING_DURATION)
+    await bt.receive_measurements(duration=sampling_duration)
 
 if __name__ == "__main__":
     asyncio.run(main())
