f6610f9f02890afc4065f24d446f6a2e6b0ffe12
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-11-26 21:42:11 -0600

    retrieve and use configuration parameters

--- src/pc/processing/main.py
+++ src/pc/processing/main.py
@@ -40,13 +40,20 @@ async def main():
                 Configuration
                 Is Pull-Down: {}
                 Series Resistance (Ω): {}
+                ADC Bitsize: {}
+                Reference Voltage: {:.2f}
+                Sample Period (sec): {} 
+
                 """.format(
                     setup[0],
                     therm[0],
                     therm[1],
                     therm[2],
-                    config[0],
-                    config[1]
+                    config[1],
+                    config[2],
+                    config[3],
+                    config[4],
+                    config[5]
                 )
             )
 
@@ -68,7 +75,7 @@ async def main():
 
         if (add_new_therm) or (input("Choose existing thermistor?: ").lower() == "no"):
             print("Adding new thermistor record to database")
-            part_number = input("Enter part number: ")
+            part_number = input("Enter new part number: ")
             therm_specs_obtained = False
             therm = db.get_single_thermistor_record(part_number)
             if therm != None:
@@ -105,9 +112,13 @@ async def main():
 
         if (add_new_config) or (input("Choose existing configuration?: ").lower() == "no"):
             print("Adding new configuration record to database")
+            config_id = input("Enter new configuration ID: ")
             is_pull_down = True if input("Thermistor is pull-down?: ").lower()=="yes" else False
             series_resistance = int(input("Enter series resistance (Ω): "))
-            db.add_config_record(is_pull_down, series_resistance)
+            adc_bitsize = int(input("Enter ADC bitsize: "))
+            v_ref = float(input("Enter Reference Voltage"))
+            sample_period_s = int(input("Enter Sample Period (seconds): "))
+            db.add_config_record(config_id, is_pull_down, series_resistance, adc_bitsize, v_ref, sample_period_s)
             config = db.get_single_config_record(is_pull_down_therm=is_pull_down, series_resistance=series_resistance)
             config_id = config[0]
         else:
@@ -115,6 +126,9 @@ async def main():
             config = db.get_single_config_record(config_id=config_id)
             is_pull_down = config[1]
             series_resistance = config[2]
+            adc_bitsize = config[3]
+            v_ref = config[4]
+            sample_period_s = config[5]
 
         db.add_setup_record(part_number=part_number, config_id=config_id)
         setup = db.get_single_setup_record(part_number=part_number, config_id=config_id)
@@ -133,9 +147,26 @@ async def main():
         res_at_25C = therm[2]
         is_pull_down = config[1]
         res_at_25C = config[2]
+        adc_bitsize = config[3]
+        v_ref = config[4]
+        sample_period_s = config[5]
 
     bt = ClientBT(device_name=_DEVICE_NAME, analog_uuid=_ANALOG_UUID)
     await bt.receive_measurements(duration=sampling_duration)
+
+    # Convert analog measurements
+    data_deg_f = []
+    for data_point in bt.data:
+        data_deg_f.append(
+            dp.convert_temp_f(
+                data_point,
+                v_ref,
+                adc_bitsize,
+                series_resistance,
+                beta,
+                is_pull_down
+            )
+        )
 
     db.add_data_record(duration=sampling_duration, degF_points=bt.data, setup_id=setup_id)
     db.disconnect()
