e898cb41f25d53df53ba788e3fd3b92b33dae33c
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-12-10 19:10:15 -0600

    display log function on plot

--- src/desktop/graphing/data_graphing.py
+++ src/desktop/graphing/data_graphing.py
@@ -17,14 +17,15 @@ def _get_decay_params(data: list[float], sample_period_s) -> tuple[float, float,
 def plot_temp_log_func(data: list[float], duration_s: int):
     sample_period_s = duration_s / len(data)
     a, b, amb_temp = _get_decay_params(data, sample_period_s)
-    print("{:.2f}*exp(-{:.5f}*t) + {:.2f}".format(a, b, amb_temp))
+    function_string = "{:.2f}*exp(-{:.5f}*t) + {:.2f}".format(a, b, amb_temp)
+    print(function_string)
     t = np.linspace(0, duration_s, duration_s * 10)
     temp = a*np.exp(-b*t) + amb_temp
 
     plt.plot(t, temp)
     plt.xlabel('Time (seconds)', fontsize=16)
     plt.ylabel('Temperature (degF)', fontsize=16)
-    plt.title('Logarithmic Curve Fit Function')
+    plt.title('Curve Fit Function: {}'.format(function_string))
     plt.grid(True)
     plt.show()
 
